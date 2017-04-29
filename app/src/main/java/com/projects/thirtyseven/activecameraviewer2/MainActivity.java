191adb771acb267b92d2fb3fd0d4015a5b32a9e4
NismoSkyline <nismoskyr34@gmail.com>
2017-04-30 02:44:49 +0600

    Add icons and LayoutButtons

--- app/src/main/java/com/projects/thirtyseven/activecameraviewer2/MainActivity.java
+++ app/src/main/java/com/projects/thirtyseven/activecameraviewer2/MainActivity.java
@@ -3,11 +3,28 @@ package com.projects.thirtyseven.activecameraviewer2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
+    FirebaseDatabase database;
+    DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+        setOnClickListeners();
+    }
+
+    private void setOnClickListeners() {
+
+    }
+
+    private void init() {
+        database = FirebaseDatabase.getInstance();
+        databaseReference = database.getReference("Camera");
     }
 }
