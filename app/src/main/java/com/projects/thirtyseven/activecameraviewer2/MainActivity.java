697800b5f3845e9d7c6dc24560764d85975c35ca
NismoSkyline <nismoskyr34@gmail.com>
2017-04-30 02:51:43 +0600

    Create fields and set Listeners

--- app/src/main/java/com/projects/thirtyseven/activecameraviewer2/MainActivity.java
+++ app/src/main/java/com/projects/thirtyseven/activecameraviewer2/MainActivity.java
@@ -2,6 +2,9 @@ package com.projects.thirtyseven.activecameraviewer2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -9,6 +12,9 @@ import com.google.firebase.database.FirebaseDatabase;
 public class MainActivity extends AppCompatActivity {
     FirebaseDatabase database;
     DatabaseReference databaseReference;
+    FrameLayout cameraLinear1, cameraLinear2, cameraLinear3, cameraLinear4, cameraLinear5, cameraLinear6;
+    ImageView imageView1, imageView2, imageView3, imageView4, imageView5, imageView6;
+    View.OnClickListener onClickListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +26,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setListeners() {
-
+        cameraLinear1.setOnClickListener(onClickListener);
+        cameraLinear2.setOnClickListener(onClickListener);
+        cameraLinear3.setOnClickListener(onClickListener);
+        cameraLinear4.setOnClickListener(onClickListener);
+        cameraLinear5.setOnClickListener(onClickListener);
+        cameraLinear6.setOnClickListener(onClickListener);
     }
 
     private void init() {
