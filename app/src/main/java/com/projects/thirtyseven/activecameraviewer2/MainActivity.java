e404677041452e5918aa652b4cc8a943e5d1ab36
NismoSkyline <nismoskyr34@gmail.com>
2017-05-01 20:49:32 +0600

    Turn bunch of variables into ArrayLists

--- app/src/main/java/com/projects/thirtyseven/activecameraviewer2/MainActivity.java
+++ app/src/main/java/com/projects/thirtyseven/activecameraviewer2/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.GridLayout;
 import android.widget.ImageView;
 
 import com.google.firebase.database.ChildEventListener;
@@ -12,22 +13,18 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     FirebaseDatabase database;
     DatabaseReference databaseReference;
-    FrameLayout cameraFrameLayoutButton1,
-            cameraFrameLayoutButton2,
-            cameraFrameLayoutButton3,
-            cameraFrameLayoutButton4,
-            cameraFrameLayoutButton5,
-            cameraFrameLayoutButton6;
-    ImageView imageStatusCamera1,
-            imageStatusCamera2,
-            imageStatusCamera3,
-            imageStatusCamera4,
-            imageStatusCamera5,
-            imageStatusCamera6;
+
+    GridLayout cameraGridLayout;
+    ArrayList<FrameLayout> cameraFrameLayoutButtons;
+    ArrayList<ImageView> imageStatusCameras;
+
     View.OnClickListener onClickListener;
+
     private CameraController cameraController;
     private CameraView cameraView;
     private CameraModel cameraModel;
@@ -39,7 +36,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         init();
-        setListeners();
 
         cameraView.update(cameraModel);
 
@@ -69,30 +65,17 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void setListeners() {
-        cameraFrameLayoutButton1.setOnClickListener(onClickListener);
-        cameraFrameLayoutButton2.setOnClickListener(onClickListener);
-        cameraFrameLayoutButton3.setOnClickListener(onClickListener);
-        cameraFrameLayoutButton4.setOnClickListener(onClickListener);
-        cameraFrameLayoutButton5.setOnClickListener(onClickListener);
-        cameraFrameLayoutButton6.setOnClickListener(onClickListener);
-    }
-
     private void init() {
         database = FirebaseDatabase.getInstance();
         databaseReference = database.getReference("Camera");
-        imageStatusCamera1 = (ImageView) findViewById(R.id.imageStatusCamera1);
-        imageStatusCamera2 = (ImageView) findViewById(R.id.imageStatusCamera2);
-        imageStatusCamera3 = (ImageView) findViewById(R.id.imageStatusCamera3);
-        imageStatusCamera4 = (ImageView) findViewById(R.id.imageStatusCamera4);
-        imageStatusCamera5 = (ImageView) findViewById(R.id.imageStatusCamera5);
-        imageStatusCamera6 = (ImageView) findViewById(R.id.imageStatusCamera6);
-        cameraFrameLayoutButton1 = (FrameLayout) findViewById(R.id.cameraFrameLayoutButton1);
-        cameraFrameLayoutButton2 = (FrameLayout) findViewById(R.id.cameraFrameLayoutButton2);
-        cameraFrameLayoutButton3 = (FrameLayout) findViewById(R.id.cameraFrameLayoutButton3);
-        cameraFrameLayoutButton4 = (FrameLayout) findViewById(R.id.cameraFrameLayoutButton4);
-        cameraFrameLayoutButton5 = (FrameLayout) findViewById(R.id.cameraFrameLayoutButton5);
-        cameraFrameLayoutButton6 = (FrameLayout) findViewById(R.id.cameraFrameLayoutButton6);
+
+        cameraGridLayout = (GridLayout) findViewById(R.id.cameraGridLayout);
+        for (int i = 0; i < cameraGridLayout.getChildCount(); i++){
+            FrameLayout button = (FrameLayout) cameraGridLayout.getChildAt(i);
+            cameraFrameLayoutButtons.add(button);
+            button.setOnClickListener(onClickListener);
+            imageStatusCameras.add((ImageView) button.getChildAt(0));
+        }
 
         cameraModel = new CameraModel();
         cameraController = new CameraController(cameraModel);
