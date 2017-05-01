006d1e3c2d40e4d54e8ca9b07ad0c14b232bdcdd
NismoSkyline <nismoskyr34@gmail.com>
2017-05-01 22:04:47 +0600

    Comment all actions

--- app/src/main/java/com/projects/thirtyseven/activecameraviewer2/MainActivity.java
+++ app/src/main/java/com/projects/thirtyseven/activecameraviewer2/MainActivity.java
@@ -6,11 +6,13 @@ import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.GridLayout;
 import android.widget.ImageView;
+
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     GridLayout cameraGridLayout;
-    ArrayList<FrameLayout> cameraFrameLayoutButtons;
+    ArrayList<FrameLayout> cameraFrameLayoutButtons; //ArrayList в котором будут находится кнопки
+    ArrayList<String> listOfCameraNames;  //ArrayList в котором будут находится имена камер
 
     View.OnClickListener onClickListener;
 
@@ -25,37 +27,54 @@ public class MainActivity extends AppCompatActivity {
 
         init();
 
-        refreshView();
+        refreshView();  //Мы используем MainActivity как Представление следоватьельно мы должны его обновить
 
-        onClickListener = new View.OnClickListener() {
+        onClickListener = new View.OnClickListener() { //МультиЛистенер т.к. мы имеем множество кнопок
             @Override
             public void onClick(View v) {
                 cameraController.clickCamera(String.valueOf(v.getId()));
-                refreshView();
+                //При нажатии контроллер мы передаём String'овое значение т.к. название камеры и Id совпадают
+                refreshView(); //Обновляем View
             }
         };
     }
 
     private void refreshView() {
-        for (FrameLayout button : cameraFrameLayoutButtons){
-            ImageView image = (ImageView)button.getChildAt(0);
+        //В методе For each : - двоеточие используется для Изьятия единного из множества
+        // cameraFrameLayoutButtons - список всех кнопок
+        for (FrameLayout button : cameraFrameLayoutButtons) {
+            ImageView image = (ImageView) button.getChildAt(0);
+            // Мы определили Image для более удоьного использования в дальнейшем.
+            //getChildAt - получение с FrameLayout ребенка под индексом 0. В нашем случае это Картинка Статуса камеры
             int cameraStatus = cameraModel.getCameraStatus(String.valueOf(button.getId()));
+            //Определяем статус взяв с модели Id.
+
+            //В зависимости от статуса меняем картинку статуса камеры
             if (cameraStatus == 0) image.setImageResource(R.drawable.grey_status);
             else if (cameraStatus == 1) image.setImageResource(R.drawable.yellow_status);
-            else if ( cameraStatus == 2) image.setImageResource(R.drawable.red_status);
+            else if (cameraStatus == 2) image.setImageResource(R.drawable.red_status);
         }
     }
 
     private void init() {
         cameraFrameLayoutButtons = new ArrayList<>();
-        ArrayList<String> listOfCameraNames = new ArrayList<>();
+        listOfCameraNames = new ArrayList<>();
+        //Инициализируем наш ArrayList'ы
 
         cameraGridLayout = (GridLayout) findViewById(R.id.cameraGridLayout);
-        for (int i = 0; i < cameraGridLayout.getChildCount(); i++){
+        //Layout с которым мы работаем
+
+        //Цикл благодаря которому мы можем инициализировать множество элементов и засетить на них Listener'ы
+        //Не засоряя код кучей повторяющихся строчек
+        for (int i = 0; i < cameraGridLayout.getChildCount(); i++) {
+            //С layout'а мы получаем количество детей, и по количеству выполняем цикл
             FrameLayout button = (FrameLayout) cameraGridLayout.getChildAt(i);
+            //Получаем ребёнка по его индексу
             cameraFrameLayoutButtons.add(button);
             listOfCameraNames.add(String.valueOf(button.getId()));
+            //Добавили элемент в ArrayList
             button.setOnClickListener(onClickListener);
+            //Добавили Listener
         }
 
         cameraModel = new CameraModel(listOfCameraNames);
