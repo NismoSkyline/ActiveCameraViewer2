c98cc93260dc05afd41b6a1f58149bf7e08dbbf7
NismoSkyline <nismoskyr34@gmail.com>
2017-05-02 19:20:55 +0600

    Remove cameraView. Clean code

--- app/src/main/java/com/projects/thirtyseven/activecameraviewer2/CameraModel.java
+++ app/src/main/java/com/projects/thirtyseven/activecameraviewer2/CameraModel.java
@@ -1,8 +1,23 @@
 package com.projects.thirtyseven.activecameraviewer2;
 
+import java.util.ArrayList;
+
 /**
  * Created by ThirtySeven on 01-May-17.
  */
 
 class CameraModel {
+    private String cameraTrigger;
+
+    public CameraModel(ArrayList<String> listOfCameraNames) {
+
+    }
+
+    public int getCameraStatus(String s) {
+        return 0;
+    }
+
+    public void setCameraTrigger(String cameraTrigger) {
+        this.cameraTrigger = cameraTrigger;
+    }
 }
